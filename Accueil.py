9e5c90da780c7c1d3790ece36a1e175d1ee8dddd
Florian Lemarchand <17027960+FlorianLemarchand@users.noreply.github.com>
2025-02-26 14:24:54 +0000

    update acceuil

--- Accueil.py
+++ Accueil.py
@@ -10,8 +10,10 @@ st.set_page_config(page_title="FFCK Descente - Non-Officiels",
 st.markdown(
 """
     Cette interface a pour but de permettre l'accès aux classements FFCK descente 
-    en attendant la refonte de la page descente FFCK. Ces classemments sont officieux. Seules
-    les informations disponibles sur ffck.org ont un caractère officiel. 
-    """
+    en attendant la refonte de la page descente FFCK. Ces classements sont officieux. Seules
+    les informations disponibles sur le site [FFCK](https://www.ffck.org/descente/) 
+    ont un caractère officiel.
+    
+"""
 )
 
